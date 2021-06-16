44647156cddf54fe936d851192c2b4b62fca8e4e
TayuyaGate <marcelosilvafreitas12@gmail.com>
2021-06-16 14:55:07 -0300

    Storage Control

--- services/frontend/index.js
+++ services/frontend/index.js
@@ -1,3 +1,29 @@
+const estoque = [{'mensagem': 'Disponível em estoque: ','quantidade':2},
+                 {'mensagem': 'Disponível em estoque: ','quantidade':2},
+                 {'mensagem': 'Disponível em estoque: ','quantidade':2}]
+
+function adicionarLivros(){
+    for(i = 0; i < estoque.length;i++){
+        estoque[i].quantidade = 5
+    }
+}
+
+function controleEstoque(id){
+    if(estoque[id-1].quantidade == 'Produto Indisponível'){
+        estoque[id-1].mensagem = ''  
+        estoque[id-1].quantidade = 'Produto Indisponível'   
+    }else{
+        estoque[id-1].quantidade = estoque[id-1].quantidade - 1  
+        if(estoque[id-1].quantidade == 0){
+            estoque[id-1].mensagem = ''  
+            estoque[id-1].quantidade = 'Produto Indisponível'   
+        }
+    }
+    const books = document.querySelector('.books');
+    books.innerHTML = ''
+    start()
+}
+
 function newBook(book) {
     const div = document.createElement('div');
     div.className = 'column is-4';
@@ -16,7 +42,7 @@ function newBook(book) {
                 <div class="content book" data-id="${book.id}">
                     <div class="book-meta">
                         <p class="is-size-4">R$${book.price.toFixed(2)}</p>
-                        <p class="is-size-6">Disponível em estoque: ${book.quantity}</p>
+                        <p class="is-size-6 estoque">${estoque[book.id-1].mensagem + estoque[book.id-1].quantidade}</p>
                         <h4 class="is-size-3 title">${book.name}</h4>
                         <p class="subtitle">${book.author}</p>
                     </div>
@@ -28,11 +54,11 @@ function newBook(book) {
                             <a class="button button-shipping is-info" data-id="${book.id}"> Calcular Frete </a>
                         </div>
                     </div>
-                    <button class="button button-buy is-success is-fullwidth">Comprar</button>
+                    <button class="button button-buy is-success is-fullwidth" data-id="${book.id}">Comprar</button>
                 </div>
             </div>
         </div>`;
-    return div;
+        return div;
 }
 
 function calculateShipping(id, cep) {
@@ -53,6 +79,11 @@ function calculateShipping(id, cep) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+    adicionarLivros()
+    start()  
+});
+
+function start(){
     const books = document.querySelector('.books');
 
     fetch('http://localhost:3000/products')
@@ -78,6 +109,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 document.querySelectorAll('.button-buy').forEach((btn) => {
                     btn.addEventListener('click', (e) => {
+                        const id = e.target.getAttribute('data-id');
+                        controleEstoque(id)
                         swal('Compra de livro', 'Sua compra foi realizada com sucesso', 'success');
                     });
                 });
@@ -87,4 +120,6 @@ document.addEventListener('DOMContentLoaded', function () {
             swal('Erro', 'Erro ao listar os produtos', 'error');
             console.error(err);
         });
-});
+}
+
+
